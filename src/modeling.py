3182722d17266180046c5d77ad376a3043ea0de0
Kirill <sokolov1779@gmail.com>
2026-06-16 20:48:06 +0300

    submissions

---
+++ src/modeling.py
@@ -0,0 +1,148 @@
+import lightgbm
+import pandas as pd
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path.cwd().parent
+sys.path.append(str(PROJECT_ROOT))
+
+from src.preprocessing import *
+from src.data_split import *
+from src.metrics import *
+from src.modeling import *
+
+def run_boosting_cv(
+    train_df,
+    time_folds,
+    split_fold,
+    model_factory,
+    model_name,
+    booster,
+    preprocessing_func,
+    selected_features=None,
+):
+    oof_pred = []
+    rmsle_by_folds = []
+    features_import = []
+    models = []
+    model_features = []
+
+    for fold in time_folds:
+        train, val = split_fold(df=train_df, fold=fold)
+
+        train, val = preprocessing_func(train), preprocessing_func(val)
+
+        if selected_features is None:
+            X_train = train.drop("price_doc", axis=1)
+            X_val = val.drop("price_doc", axis=1)
+        else:
+            X_train = train[selected_features]
+            X_val = val[selected_features]
+
+        y_train = train["price_doc"]
+        y_val = val["price_doc"]
+
+        val_ids = val.index.copy()
+
+        cat_features = X_train.select_dtypes(
+            include="category"
+        ).columns.to_list()
+
+        model = model_factory
+
+        if booster == "lightgbm":
+            model.fit(
+                X_train,
+                y_train,
+                eval_set=[(X_val, y_val)],
+                eval_metric="rmse",
+                categorical_feature=cat_features,
+                callbacks=[
+                    lightgbm.early_stopping(stopping_rounds=200),
+                    lightgbm.log_evaluation(period=200),
+                ],
+            )
+
+            pred_log = model.predict(
+                X_val,
+                num_iteration=model.best_iteration_
+            )
+
+            feature_importance = model.feature_importances_
+
+        elif booster == "xgboost":
+            model.fit(
+                X_train,
+                y_train,
+                eval_set=[(X_val, y_val)],
+                verbose=200,
+            )
+
+            pred_log = model.predict(X_val)
+
+            feature_importance = model.feature_importances_
+
+        elif booster == "catboost":
+            model.fit(
+                X_train,
+                y_train,
+                cat_features=cat_features,
+                eval_set=(X_val, y_val),
+                use_best_model=True,
+            )
+
+            pred_log = model.predict(X_val)
+
+            feature_importance = model.get_feature_importance()
+
+        else:
+            raise ValueError(
+                "booster должен быть: 'lightgbm', 'xgboost' или 'catboost'"
+            )
+
+        pred = np.expm1(pred_log)
+
+        score = rmsle(np.expm1(y_val), pred)
+        error = pred - np.expm1(y_val)
+
+        rmsle_by_folds.append(score)
+
+        models.append({
+            "booster": booster,
+            "fold": fold["fold"],
+            "model": model,
+            "features": X_train.columns.to_list()
+        })
+
+        features_import.append(feature_importance)
+        model_features.append(X_train.columns.to_list())
+
+        fold_oof = pd.DataFrame({
+            "id": val_ids,
+            "fold": fold["fold"],
+            "val_block": fold["val_block"],
+            "y_true": np.expm1(y_val).values,
+            f"{model_name}_pred": pred,
+            f"{model_name}_error": error
+        })
+
+        oof_pred.append(fold_oof)
+
+    quality = pd.DataFrame([{
+        "model_name": model_name,
+        "fold_1": rmsle_by_folds[0],
+        "fold_2": rmsle_by_folds[1],
+        "fold_3": rmsle_by_folds[2],
+    }])
+
+    quality["mean_rmsle"] = quality[
+        ["fold_1", "fold_2", "fold_3"]
+    ].mean(axis=1)
+
+    quality["rmsle_std"] = quality[
+        ["fold_1", "fold_2", "fold_3"]
+    ].std(axis=1)
+
+    oof_pred = pd.concat(oof_pred)
+
+    return quality, oof_pred, models, features_import, model_features
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,19 +2,40 @@ import pandas as pd
 import numpy as np
 
 def timestamp_prep(df: pd.DataFrame):
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df['year'] = df['timestamp'].dt.year
-    df['month'] = df['timestamp'].dt.month
-    df['day'] = df['timestamp'].dt.year
-    df = df.drop('timestamp', axis=1)
-    df = df.drop('time_block', axis=1)
+    if 'timestamp' in df.columns:
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
+        df['year'] = df['timestamp'].dt.year
+        df['month'] = df['timestamp'].dt.month
+        df['day'] = df['timestamp'].dt.year
+        df = df.drop('timestamp', axis=1)
+    if 'time_block' in df.columns:
+        df = df.drop('time_block', axis=1)
     
     return df
+
+def features_engennering(df: pd.DataFrame):
+    df["living_ratio"] = df["life_sq"] / df["full_sq"]
+    df["kitchen_ratio"] = df["kitch_sq"] / df["full_sq"]
+    df["non_living_sq"] = df["full_sq"] - df["life_sq"]
+    df["non_living_ratio"] = df["non_living_sq"] / df["full_sq"]
+
+    df["sq_per_room"] = df["full_sq"] / df["num_room"]
+    df["life_sq_per_room"] = df["life_sq"] / df["num_room"]
+
+    df["rel_floor"] = df["floor"] / df["max_floor"]
+    df["is_first_floor"] = (df["floor"] == 1).astype(int)
+    df["is_top_floor"] = (df["floor"] == df["max_floor"]).astype(int)
+    df["floor_from_top"] = df["max_floor"] - df["floor"]
+
+    df["house_age"] = df["year"] - df["build_year"]
+
+    return df
+
 
 def anomalies_deleting(df: pd.DataFrame):
     df_cleaned = df.copy()
     df_cleaned = df_cleaned.drop_duplicates()
-    for i in ['price_doc', 'full_sq', 'life_sq', 'floor', 'max_floor', 'build_year', 'num_room', 'state', 'raion_popul']:
+    for i in ['full_sq', 'life_sq', 'floor', 'max_floor', 'build_year', 'num_room', 'state', 'raion_popul']:
         df_cleaned.loc[df_cleaned[i] == 0, i] = np.nan
 
     df_cleaned.loc[df_cleaned['max_floor'] < df_cleaned['floor'], ['max_floor', 'floor']] = np.nan
@@ -32,7 +53,8 @@ def anomalies_deleting(df: pd.DataFrame):
     return df_cleaned
 
 def log_target(df: pd.DataFrame, target = 'price_doc'):
-    df[target] = np.log1p(df[target])
+    if 'price_doc' in df.columns:
+        df[target] = np.log1p(df[target])
 
     return df
 
@@ -40,7 +62,6 @@ def cat_features_encoding(df: pd.DataFrame):
     cat_features = df.select_dtypes(include=['object', 'datetime'])
     for i in cat_features:
         df[i] = df[i].astype('category')
-    
     return df
 
 def base_preprocessing(df: pd.DataFrame):
@@ -50,3 +71,12 @@ def base_preprocessing(df: pd.DataFrame):
     df = cat_features_encoding(df=df)
 
     return df
+
+def prep_with_features_eng(df: pd.DataFrame):
+    df = anomalies_deleting(df=df)
+    df = timestamp_prep(df=df)
+    df = log_target(df=df)
+    df = features_engennering(df=df)
+    df = cat_features_encoding(df=df)
+    return df
+    
